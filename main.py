c55425fcde904a8509b6a1b998c68ca8bb33539a
Felipe <felipegijsen12@gmail.com>
2026-06-21 23:45:58 -0300

    update

--- main.py
+++ main.py
@@ -31,7 +31,8 @@ app.add_middleware(
         "http://localhost:5500",      # Live Server
         "http://127.0.0.1:5500",
         "http://localhost:8000",
-        "http://127.0.0.1:8000"
+        "http://127.0.0.1:8000",
+        "https://projeto-nave.github.io/POC/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
