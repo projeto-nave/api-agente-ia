ad2b1407c47f84313a08f790bc44e2af59bcc3f3
Felipe <felipegijsen12@gmail.com>
2026-06-21 21:17:12 -0300

    update

--- startup.sh
+++ startup.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+# Instala as dependências
+echo "Instalando dependências..."
+pip install --no-cache-dir -r requirements.txt
+
+# Inicia a aplicação
+echo "Iniciando aplicação..."
 uvicorn main:app --host 0.0.0.0 --port 8000
