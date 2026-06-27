35dbf95b7d8a64c1c25f8ea8aafd19bd4fe3b530
Felipe <felipegijsen12@gmail.com>
2026-06-27 00:11:45 -0300

    update

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ app.add_middleware(
         "http://127.0.0.1:5500",
         "http://localhost:8000",
         "http://127.0.0.1:8000",
-        "https://projeto-nave.github.io"
-        "https://projeto-nave.github.io/POC/#"
+        "https://projeto-nave.github.io",
+        "https://projeto-nave.github.io/POC/#",
         "https://projeto-nave.github.io/POC/"
     ],
     allow_credentials=True,
